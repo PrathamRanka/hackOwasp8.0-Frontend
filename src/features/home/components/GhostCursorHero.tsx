f10d96295245bd9eabd0c12290ad5e53203e6671
Pratham <pranka0789@gmail.com>
2026-03-15 16:45:11 +0530

    updated color

--- src/features/home/components/GhostCursorHero.tsx
+++ src/features/home/components/GhostCursorHero.tsx
@@ -4,7 +4,23 @@ import { useState } from "react";
 
 import GhostCursor from "@/components/ui/effects/GhostCursor";
 
-const CURSOR_COLORS = ["#6FE7DD", "#FFD6A5", "#A8E6CF", "#FFAFCC", "#BDE0FE"];
+const CURSOR_COLORS = [
+  "#6FE7DD", // aqua
+  "#FFD6A5", // peach
+  "#A8E6CF", // mint
+  "#FFAFCC", // soft pink
+  "#BDE0FE", // sky blue
+  "#FFE066", // pastel yellow
+  "#FFB4A2", // coral
+  "#FEC5BB", // blush
+  "#E0BBE4", // lavender
+  "#CDB4DB", // soft purple
+  "#B9FBC0", // light green
+  "#FFD3B6", // soft orange
+  "#FFADAD", // light red
+  "#CAE9FF", // pale blue
+  "#FFF1A8"  // creamy yellow
+];
 
 export default function GhostCursorHero() {
   const [randomCursorColor] = useState(
